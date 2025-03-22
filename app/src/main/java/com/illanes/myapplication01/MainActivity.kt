a0caaeceb78e4879da27771cfe03f4ea02f3aa18
Christian <christian.illanes@gmail.com>
2025-03-23 00:51:53 +0100

    Add missing import androidx.compose.runtime.getValue

--- app/src/main/java/com/illanes/myapplication01/MainActivity.kt
+++ app/src/main/java/com/illanes/myapplication01/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider

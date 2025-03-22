889517a905c48099b1524af7d290e4df2895ce28
Christian <christian.illanes@gmail.com>
2025-03-22 23:47:21 +0100

    Add changes according to guide https://www.youtube.com/watch?v=bOd3wO0uFr8

--- app/src/main/java/com/illanes/myapplication01/Contact.kt
+++ app/src/main/java/com/illanes/myapplication01/Contact.kt
@@ -1,11 +1,13 @@
 package com.illanes.myapplication01
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity
 data class Contact(
-    @PrimaryKey
-    val id: Int,
     val firstName: String,
     val lastName: String,
     val phoneNumber: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
 )
--- app/src/main/java/com/illanes/myapplication01/MainActivity.kt
+++ app/src/main/java/com/illanes/myapplication01/MainActivity.kt
@@ -4,44 +4,89 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.room.Room
 import com.illanes.myapplication01.ui.theme.MyApplication01Theme
 
+// The FULL Beginner Guide for Room in Android | Local Database Tutorial for Android
+// https://www.youtube.com/watch?v=bOd3wO0uFr8
+
 class MainActivity : ComponentActivity() {
+
+    private val db by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            ContactDatabase::class.java,
+            "contacts.db"
+        ).build()
+    }
+    private val viewModel by viewModels<ContactViewModel>(
+        factoryProducer = {
+            object : ViewModelProvider.Factory {
+                override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                    return ContactViewModel(db.dao) as T
+                }
+            }
+        }
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyApplication01Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val state by viewModel.state.collectAsState()
+                ContactScreen(state = state, onEvent = viewModel::onEvent)
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import com.illanes.myapplication01.ui.theme.MyApplication01Theme
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplication01Theme {
-        Greeting("Android")
-    }
-}
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContent {
+//            MyApplication01Theme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    MyApplication01Theme {
+//        Greeting("Android")
+//    }
+//}

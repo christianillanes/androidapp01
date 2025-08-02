bfffe5c9d8aed817cd1a8e9b55263095c3473b0f
Christian <christian.illanes@gmail.com>
2025-08-02 08:35:43 +0200

    Add BasicAlertDialog

--- app/src/main/java/com/illanes/myapplication01/AddContactDialog.kt
+++ app/src/main/java/com/illanes/myapplication01/AddContactDialog.kt
@@ -1,17 +1,20 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.illanes.myapplication01
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun AddContactDialog(
@@ -19,56 +22,59 @@ fun AddContactDialog(
     onEvent: (ContactEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
-//    AlertDialog(
-//        modifier = modifier,
-//        onDismissRequest = {
-//            onEvent(ContactEvent.HideDialog)
-//        },
-//        title = { Text(text = "Add contact") },
-//        text = {
-//            Column(
-//                verticalArrangement = Arrangement.spacedBy(8.dp)
-//            ) {
-//                TextField(
-//                    value = state.firstName,
-//                    onValueChange = {
-//                        onEvent(ContactEvent.SetFirstName(it))
-//                    },
-//                    placeholder = {
-//                        Text(text = "First name")
-//                    }
-//                )
-//                TextField(
-//                    value = state.lastName,
-//                    onValueChange = {
-//                        onEvent(ContactEvent.SetLastName(it))
-//                    },
-//                    placeholder = {
-//                        Text(text = "Last name")
-//                    }
-//                )
-//                TextField(
-//                    value = state.phoneNumber,
-//                    onValueChange = {
-//                        onEvent(ContactEvent.SetPhoneNumber(it))
-//                    },
-//                    placeholder = {
-//                        Text(text = "Phone number")
-//                    }
-//                )
-//            }
-//        },
-//        buttons = {
-//            Box(
-//                modifier = Modifier.fillMaxWidth(),
-//                contentAlignment = Alignment.CenterEnd
-//            ) {
-//                Button(onClick = {
-//                    onEvent(ContactEvent.SaveContact)
-//                }) {
-//                    Text(text = "Save")
-//                }
-//            }
-//        }
-//    )
+
+    BasicAlertDialog(
+        onDismissRequest = {
+            onEvent(ContactEvent.HideDialog)
+        },
+        modifier = modifier,
+        properties = DialogProperties(),
+        content = {
+            Column(
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                TextField(
+                    value = "Add contact",
+                    onValueChange = {}
+                )
+                TextField(
+                    value = state.firstName,
+                    onValueChange = {
+                        onEvent(ContactEvent.SetFirstName(it))
+                    },
+                    placeholder = {
+                        Text(text = "First name")
+                    }
+                )
+                TextField(
+                    value = state.lastName,
+                    onValueChange = {
+                        onEvent(ContactEvent.SetLastName(lastName = it))
+                    },
+                    placeholder = {
+                        Text(text = "Last name")
+                    }
+                )
+                TextField(
+                    value = state.phoneNumber,
+                    onValueChange = {
+                        onEvent(ContactEvent.SetPhoneNumber(phoneNumber = it))
+                    },
+                    placeholder = {
+                        Text(text = "Phone number")
+                    }
+                )
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.CenterEnd
+                ) {
+                    Button(onClick = {
+                        onEvent(ContactEvent.SaveContact)
+                    }) {
+                        Text(text = "Save")
+                    }
+                }
+            }
+        }
+    )
 }
